f70742241409b1f40e56e44528b45bc3465d3d58
Valerio Lopez Miguel <valerio.lopez@outlook.com>
2024-07-03 12:16:53 -0600

    funcionalidad de prueba tecnica del lado cliente

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Identifica;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/identificar', [Identifica::class, 'index']);
